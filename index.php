9c6020e593f468c3365dd6c262e34627463f563f
Hennek <zenbom@gmail.com>
2013-10-16 12:17:25 +0200

    Mise à jour de la structure

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
 
 /**
  *
- * ServerStatus : Garder un oeil sur le status de vos serveurs à tout moment
+ *        _____                          _____ _        _
+ *       /  ___|                        /  ___| |      | |
+ *       \ `--.  ___ _ ____   _____ _ __\ `--.| |_ __ _| |_ _   _ ___
+ *        `--. \/ _ \ '__\ \ / / _ \ '__|`--. \ __/ _` | __| | | / __|
+ *       /\__/ /  __/ |   \ V /  __/ |  /\__/ / || (_| | |_| |_| \__ \
+ *       \____/ \___|_|    \_/ \___|_|  \____/ \__\__,_|\__|\__,_|___/
  *
- * Créé par Hennek, design inspiré de MCstatus (http://xpaw.ru/mcstatus/) et
- * de la future version du site de Hennek (http://hennek.be)
+ * ServerStatus : Garder un oeil sur le status de vos serveurs à tout moment
+ * Créé par Hennek, design inspiré de MCstatus (http://xpaw.ru/mcstatus/)
  *
  * @name         ServerStatus
  * @version      1.0
@@ -13,17 +18,35 @@
  * @project      https://github.com/Hennek/ServerStatus
  * @licence      Beerware (https://fr.wikipedia.org/wiki/Beerware)
  *
- *
- *    _____                          _____ _        _
- *   /  ___|                        /  ___| |      | |
- *   \ `--.  ___ _ ____   _____ _ __\ `--.| |_ __ _| |_ _   _ ___
- *    `--. \/ _ \ '__\ \ / / _ \ '__|`--. \ __/ _` | __| | | / __|
- *   /\__/ /  __/ |   \ V /  __/ |  /\__/ / || (_| | |_| |_| \__ \
- *   \____/ \___|_|    \_/ \___|_|  \____/ \__\__,_|\__|\__,_|___/
- *
  */
 
-    // FONCTION
+    /**
+     *
+     */
+    function readFileConfig() {
+
+    }
+
+    /**
+     *
+     */
+    function addLineConfig() {
+
+    }
+
+    /**
+     *
+     */
+    function deleteLineConfig() {
+
+    }
+
+    /**
+     *
+     */
+    function secure($str) {
+        return $str;
+    }
 
     /**
      * Ping ... Pong :)
@@ -36,14 +59,11 @@
         $starttime = microtime(true);
         $file      = @fsockopen($domain, $port, $errno, $errstr, $timeout);
         $stoptime  = microtime(true);
-        $status    = 0;
+        $status    = -1;
 
-        // Ping ... ?
-        if (!$file) {
-            $status = -1;
-        } else {
+        if ($file) {
             fclose($file);
-            // ... Pong !
+
             $status = ($stoptime - $starttime) * 1000;
             $status = floor($status);
         }
@@ -54,6 +74,7 @@
 
     // Liste des domaines ainsi que le nom à tester
     $listOfServers = array();
+    // TODO readConfigFile
     // Syntaxe suivante :
     //   $listOfServers['name']['name']  = 'Description';
     //   $listOfServers['name']['url']   = 'www.server.tld';
@@ -65,19 +86,28 @@
     // Préparation de l'affichage
     // Calcul de la largeur des bulles
     $cptServer = count($listOfServers);
-    if($cptServer == 1)
-        $width = "width: 100%; *width: 100%;";
-    elseif($cptServer == 2)
-        $width = "width: 50%; *width: 50%;";
-    elseif($cptServer % 3 == 0 && $cptServer % 4 != 0)
-        $width = "width: 33%; *width: 33%;";
-    else
-        $width = "width: 25%; *width: 25%;";
+    if($cptServer == 1) $width = "width: 100%; *width: 100%;";
+    elseif($cptServer == 2) $width = "width: 50%; *width: 50%;";
+    elseif($cptServer % 3 == 0 && $cptServer % 4 != 0)  $width = "width: 33%; *width: 33%;";
+    else $width = "width: 25%; *width: 25%;";
 
     // Création des résultats avant l'affichage
     $result = array();
     foreach ($listOfServers as $key => $value) {
         $result[$key] = pingDomain($value['url'], $value['port']);
+    }
+
+    if(isset($_GET['add'])) {
+        $add = secure($_GET['add']);
+    }
+
+    if(isset($_GET['delete'])) {
+        $delete = secure($_GET['delete']);
+    }
+
+
+    if(isset($_GET['modify'])) {
+        $modify = secure($_GET['modify']);
     }
 
     // Status - ajax - json
@@ -94,10 +124,25 @@
 <html lang="fr-FR">
 <head>
     <meta charset="utf-8" />
+    <meta name="robots" content="noindex"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
     <title>Server status • Is it down ?</title>
 
-    <link rel="stylesheet" type="text/css" href="css/default.css" />
-    <link href="img/favicon.ico" rel="icon" type="image/x-icon" />
+    <link rel="stylesheet" type="text/css" href="static/css/default.css" />
+
+    <!-- Icons -->
+    <link rel="shortcut icon" type="image/x-icon" href="static/img/favicon.ico" />
+
+    <link rel="apple-touch-icon-precomposed" media="screen and (resolution: 326dpi)" href="static/img/serverstatusx114px.png" />
+    <link rel="apple-touch-icon-precomposed" media="screen and (resolution: 163dpi)" href="static/img/serverstatusx57px.png" />
+    <link rel="apple-touch-icon-precomposed" media="screen and (resolution: 132dpi)" href="static/img/serverstatusx72px.png" />
+
+    <link rel="shortcut icon" sizes="1024x1024" href="tpl/img/serverstatusx1024.png">
+    <link rel="shortcut icon" sizes="512x512" href="tpl/img/serverstatusx512.png">
+    <link rel="shortcut icon" sizes="128x128" href="tpl/img/serverstatusx128.png">
+    <link rel="shortcut icon" sizes="114x114" href="tpl/img/serverstatusx114.png">
+    <link rel="shortcut icon" sizes="72x72" href="tpl/img/serverstatusx72.png">
 
     <style>
         .grid .col {
@@ -111,8 +156,15 @@
 
         <h1>Current status servers</h1>
 
+        <div class="add-server">
+
+        </div>
+
         <noscript>
-            <p>Javascript est désactivé. Le site ne pourra pas fonctionner.<br />Activez-le pour pouvoir en profiter !</p>
+            <p>
+                Javascript est désactivé. Le site ne pourra pas fonctionner.<br />
+                Activez-le pour pouvoir en profiter !
+            </p>
         </noscript>
 
         <div class="grid">
@@ -125,7 +177,7 @@
                 </span>
             </div>
             <div class="info-sup">
-                Temps de réponse : <span id="ms-<?php echo $key; ?>">0</span>ms
+                Temps de réponse : <span id="ms-<?php echo $key; ?>">∞</span>ms
             </div>
             </div>
             <?php endforeach; ?>
@@ -134,11 +186,13 @@
 
     <footer class="dashed clear">
         <div class="footer-right">
-            CopyLeft (Beerware) <?php echo date('Y', time()); ?> — <a href="https://twitter.com/Hennek_">Hennek</a> — v1.0<br />
-            <a href="https://github.com/Hennek/ServerStatus/issues">Report Issue</a> • <a href="https://github.com/Hennek/ServerStatus">Source</a>
+            CopyLeft (Beerware) <?php echo date('Y', time()); ?> — <a href="https://twitter.com/Hennek_">Hennek</a> — v1.1<br />
+            <a href="https://github.com/Hennek/ServerStatus/issues">Report Issue</a> •
+            <a href="https://github.com/Hennek/ServerStatus">Source</a>
         </div>
 
-        Dernière vérification : <span id="time">00:00:00</span>. Actualisation dans <span id="timer" class="note">15</span> seconde(s)<br />
+        Dernière vérification : <span id="time">00:00:00</span>.
+        Actualisation dans <span id="timer" class="note">15</span> seconde(s)<br />
         <span id="info">Tout va bien ! N'est-ce pas merveilleux ?</span>
     </footer>
 
@@ -212,14 +266,10 @@
                     }
                 });
 
-                if(countDown == 0 && countSlow == 0)
-                    info.html(msgOK);
-                if(countDown > 0)
-                    info.html("Argh, houston, we've got a problem !");
-                if(countSlow > 0)
-                    info.html("Bon, bon, le réseau est un peu lent !");
-                if(countDown > 0 && countSlow > 0)
-                    info.html("Je ne sais pas s'il faut paniquer, mais, il y a un vrai problème !");
+                if(countDown == 0 && countSlow == 0) info.html(msgOK);
+                if(countDown > 0) info.html("Argh, houston, we've got a problem !");
+                if(countSlow > 0) info.html("Bon, bon, le réseau est un peu lent !");
+                if(countDown > 0 && countSlow > 0) info.html("Je ne sais pas s'il faut paniquer, mais, il y a un vrai problème !");
             }
         });
     }
